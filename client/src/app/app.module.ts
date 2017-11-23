63cb105da636607d668ed391319a6daa1dbb087c
staticinteger <timothyjamespeacock@gmail.com>
2017-11-23 03:26:39 -0500

    Implement sidebar markup
    * Also added sidebar toggle to the menu button

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,28 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import {
 	MatToolbarModule,
 	MatButtonModule,
 	MatIconModule,
+	MatSidenavModule,
+	MatListModule
 } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+		BrowserModule,
+		BrowserAnimationsModule,
 		AppRoutingModule,
 
 		MatToolbarModule,
 		MatButtonModule,
-		MatIconModule
+		MatIconModule,
+		MatSidenavModule,
+		MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
